340608116e0b9df51061c208b20b31e20014aa32
otidlam <graduck484@gmail.com>
2017-12-08 15:57:05 +0100

    fail at main.ts but routes defined

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ih-lastProject-app
 
-User Stories:
-	Users:	
+## User Stories:
+### Users:	
 	- As user i want to be able to log in.
 	- As user i want to be able to create an event.
 	- AS user i want to be able to join an event.
 	- As user i want to be able to leave the event i joined.
 	
-Annonymous:
+### Annonymous:
 	- As Annonymous i want to be able to sign up.
 	- As Annonymous i want to be able to see the events.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components//log-in/log-in.component';
+import { HeaderComponent } from './components//header/header.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { EventComponent } from './components/event/event.component';
+
+const routes: Routes = [
+  { path: 'auth/login', component: LogInComponent },
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'main', component: AppComponent },
+  { path: 'event/_id', component: EventComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SignupComponent,
+    HeaderComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
